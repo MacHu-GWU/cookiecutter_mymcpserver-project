2baa059cda77369568b9adf64d5df6987edb04fc
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-06-21 12:02:08 -0400

    update get_weather

--- tests/test_get_weather.py
+++ tests/test_get_weather.py
@@ -5,7 +5,7 @@ from cookiecutter_mymcpserver.get_weather import get_forecast
 
 def test_get_forecast():
     text = get_forecast(38.89773371563334, -77.03653255463253)  # The White House
-    print(text)
+    # print(text)  # for debug only
 
 
 if __name__ == "__main__":
