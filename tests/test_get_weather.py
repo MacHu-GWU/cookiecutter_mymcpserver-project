ec796ac8bacdaa7dddfdef409dfc8cf6079d025a
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-06-21 11:50:59 -0400

    add get weather

---
+++ tests/test_get_weather.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from cookiecutter_mymcpserver.get_weather import get_forecast
+
+
+def test_get_forecast():
+    text = get_forecast(38.89773371563334, -77.03653255463253)  # The White House
+    print(text)
+
+
+if __name__ == "__main__":
+    from cookiecutter_mymcpserver.tests import run_cov_test
+
+    run_cov_test(
+        __file__,
+        "cookiecutter_mymcpserver.api",
+        preview=False,
+    )
